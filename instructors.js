26f04ca068620f3f6149a8b11806c23309bc934c
Luciano Barreto <contato.lucianobarreto@gmail.com>
2020-09-11 23:55:57 -0300

    Inclusão de arquivos.JS

---
+++ instructors.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const data = require('./data.json')
+const { age } = require('./date')
+
+// show
+exports.show = function(req, res) {
+    //== maneiras de receber os dados:
+    // req.query.id
+    // req.body
+    // req.params.id = /:id
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+    }
+
+    return res.render("instructors/show", { instructor })
+}
+// create
+exports.post = function(req, res) {
+    const keys = Object.keys(req.body)
+    // estrutura de validação
+    for(key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fields!')
+        }
+    }
+
+    let { avatar_url, birth, name, services, gender } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at,
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 4), function(err) {
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
+    })
+
+    // return res.send(req.body)
+}
+// update
+
+// delete
